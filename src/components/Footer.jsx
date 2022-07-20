85018826f9e6700e019b47ceb6b269f44e79194a
Valentina Cerda <valentinacerda.a@gmail.com>
2022-07-20 19:54:01 -0400

    proyecto final

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <div>
+      <footer className='bg-light text-center text-lg-start'>
+        <div className='footer text-center p-3'>© 2020 Copyright:
+          <a className='text-dark' href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+        </div>
+      </footer>
+    </div>
+  );
+};
